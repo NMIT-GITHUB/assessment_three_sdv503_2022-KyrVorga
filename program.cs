12537ccd8fd0327729772842ef43ff7315c9d3c3
KyrVorga <Rhyleitremlett@gmail.com>
2022-05-24 23:00:00 +1200

    Setup program.cs file

---
+++ program.cs
@@ -0,0 +1,14 @@
+/* File Information
+Title: Javascript conversion
+Author: Rhylei Tremlett
+Version: 1
+Date: 24/05/2022
+------------------------------------------------------------- */
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine('Hello World!');
+    }
+}

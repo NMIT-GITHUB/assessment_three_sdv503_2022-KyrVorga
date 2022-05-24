7fc3f8d5b214c3684cd4f73f6479392f281a584e
KyrVorga <Rhyleitremlett@gmail.com>
2022-05-24 23:14:16 +1200

    Creating a test program to get use to c#

--- program.cs
+++ program.cs
@@ -4,11 +4,21 @@ Author: Rhylei Tremlett
 Version: 1
 Date: 24/05/2022
 ------------------------------------------------------------- */
+using System;
 
 class Program
 {
+    string name;
+
+    public void GetName()
+    {
+        name = Console.ReadLine();
+    }
+
     static void Main()
     {
-        System.Console.WriteLine('Hello World!');
+        Program instance = new Program();
+        instance.GetName();
+        Console.WriteLine(name);
     }
 }

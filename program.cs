fbc79fdc4ef4cc0130657247300c1a9f8b3305ee
KyrVorga <Rhyleitremlett@gmail.com>
2022-06-01 11:37:23 +1200

    Created a method that generates a jagged array with random numbers

--- program.cs
+++ program.cs
@@ -11,20 +11,34 @@ class Program
     public int[][] Construct(int size)
     {
         int[][] jagged = new int[size][];
+        Random rnd = new Random();
+        for (int i = 0; i < jagged.Length; i++)
+        {
+            int col = rnd.Next(1, 10);
+            jagged[i] = new int[col];
+            for (int j = 0; j < jagged[i].Length; j++)
+            {
+                jagged[i][j] = rnd.Next(1, 100);
+            }
+        }
         return jagged;
     }
 
     static void Main()
     {
         Program p = new Program();
-        string size = Console.ReadLine();
-        int[][] result = p.Construct(Int32.Parse(size));
-
+        Console.WriteLine("Enter the size of the jagged array");
+        int size = int.Parse(Console.ReadLine());
+        int[][] result = p.Construct(size);
+        
         for (int i = 0; i < result.Length; i++)
         {
-            Console.WriteLine(result[i]);
+            Console.WriteLine("contents of sub-array: ", i);
+            for (int j = 0; j < result[i].Length; j++)
+            {
+                Console.WriteLine(result[i][j]);
+            }
         }
-        Console.WriteLine("array: "+ string.Join(",", result));
         Console.ReadLine();
     }
 }
